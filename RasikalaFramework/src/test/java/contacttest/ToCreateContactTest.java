a4f2735b8ebacf7970088ed588cac4d79fb60096
rasik <rasik@DESKTOP-HNCAMB3>
2025-06-25 11:57:43 +0530

    Frame work pushing

---
+++ RasikalaFramework/src/test/java/contacttest/ToCreateContactTest.java
@@ -0,0 +1,10 @@
+package contacttest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("Create by Rasikala");
+
+	}
+
+}
---
+++ RasikalaFramework/src/test/java/organizationtest/ToCreateOrgTest.java
@@ -0,0 +1,10 @@
+package organizationtest;
+
+public class ToCreateOrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organization created succesfully");
+
+	}
+
+}
